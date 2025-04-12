dc24214befd28a8fb023bffdf07e5d64858e27e5
ryangprince <rpprince18@gmail.com>
2025-04-11 21:21:15 -0400

    Changed admin.py in courses everything still working

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Course
 
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'access']
+    list_filter = ['status', 'access']
+    fields = ['title', 'description', 'status', 'image', 'access']
 
-admin.site.register(Course)
+
+# admin.site.register(Course, CourseAdmin)

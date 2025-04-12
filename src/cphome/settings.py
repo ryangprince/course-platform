acf5be05b465cda4c1f07a020a28062de67e05cc
ryangprince <rpprince18@gmail.com>
2025-04-11 20:46:20 -0400

    Last commit before Cloudify

--- src/cphome/settings.py
+++ src/cphome/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOCAL_CDN = BASE_DIR.parent / "local-cdn"
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = LOCAL_CDN / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

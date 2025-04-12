cbf470b2601db20a0d3313643fd79e27439bbaae
ryangprince <rpprince18@gmail.com>
2025-04-11 21:29:31 -0400

    Displaying course image everything working

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 # Register your models here.
 from .models import Course
 
@@ -7,7 +7,17 @@ from .models import Course
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['title', 'status', 'access']
     list_filter = ['status', 'access']
-    fields = ['title', 'description', 'status', 'image', 'access']
+    fields = ['title', 'description', 'status', 'image', 'access', 'display_image']
+    readonly_fields = ['display_image']
+    
+    def display_image(self, obj, *args, **kwargs):
+        print(obj.image.url)
+        url = obj.image.url
+        return format_html(f"<img src='{url}' />")
 
-
+    display_image.short_description = "Current Image"
+    
+    
+    
+    
 # admin.site.register(Course, CourseAdmin)

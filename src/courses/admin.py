955e78d40ac5b449832153ceb5066e3cda9a65f6
ryangprince <rpprince18@gmail.com>
2025-04-12 16:01:05 -0400

    CourseAdmin "inlines = [Lesson]" breaking throws error

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,19 +1,25 @@
+from cloudinary import CloudinaryImage
 from django.contrib import admin
 from django.utils.html import format_html
 # Register your models here.
-from .models import Course
+from .models import Course, Lesson
+
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    # inlines = [Lesson]
     list_display = ['title', 'status', 'access']
     list_filter = ['status', 'access']
     fields = ['title', 'description', 'status', 'image', 'access', 'display_image']
     readonly_fields = ['display_image']
     
     def display_image(self, obj, *args, **kwargs):
-        print(obj.image.url)
-        url = obj.image.url
-        return format_html(f"<img src='{url}' />")
+        url = obj.image_admin_url
+        return format_html(f"<img src={url} />")
 
     display_image.short_description = "Current Image"
     
--- src/courses/models.py
+++ src/courses/models.py
@@ -31,3 +31,57 @@ class Course(models.Model):
     @property
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
+    
+    @property
+    def image_admin_url(self):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": 200
+        }
+        url = self.image.build_url(**image_options)
+        return url
+    
+    def get_image_thumbnail(self, as_html=False, width=500):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": width
+        }
+        if as_html:
+            # CloudinaryImage(str(self.image)).image(**image_options)
+            return self.image.image(**image_options)
+        # CloudinaryImage(str(self.image)).image(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+    
+    def get_image_detail(self, as_html=False, width=750):
+        if not self.image:
+            return ""
+        image_options = {
+            "width": width
+        }
+        if as_html:
+            # CloudinaryImage(str(self.image)).image(**image_options)
+            return self.image.image(**image_options)
+        # CloudinaryImage(str(self.image)).image(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+    
+# Lesson.objects.all() # lesson queryset -> all rows
+# Lesson.objects.first()
+# course_obj = Course.objects.first()
+# Lesson.objects.filter(course_id=course_obj.id)
+# course_obj.lesson_set.all()
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    thumbnail = CloudinaryField("image", blank=True, null=True)
+    video = CloudinaryField("video", blank=True, null=True, resource_type="video")
+    can_preview = models.BooleanField(default=False, 
+                                      help_text="If user does not have access to course, can they see this?")
+    status = models.CharField(max_length=10,
+                              choices=PublishStatus.choices,
+                              default=PublishStatus.PUBLISHED)

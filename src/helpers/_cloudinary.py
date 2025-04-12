d1753bfc2f38fd9ec47bdad60381776d7fd2d828
ryangprince <rpprince18@gmail.com>
2025-04-11 21:02:40 -0400

    Installed Cloudinary and everything still working

---
+++ src/helpers/_cloudinary.py
@@ -0,0 +1,15 @@
+import cloudinary
+from decouple import config
+# Configuration  
+
+CLOUDINARY_CLOUD_NAME = config("CLOUDINARY_CLOUD_NAME", default="")
+CLOUDINARY_PUBLIC_API_KEY = config("CLOUDINARY_PUBLIC_API_KEY", default="311111347712729")
+CLOUDINARY_SECRET_API_KEY = config("CLOUDINARY_SECRET_API_KEY")
+
+def cloudinary_init():     
+    cloudinary.config( 
+        cloud_name = CLOUDINARY_CLOUD_NAME, 
+        api_key = CLOUDINARY_PUBLIC_API_KEY, 
+        api_secret = CLOUDINARY_SECRET_API_KEY, 
+        secure=True
+    )

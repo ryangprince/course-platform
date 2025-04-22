dea10168b1e754b003c4f0fa685924b8f1377bee
ryangprince <rpprince18@gmail.com>
2025-04-22 17:05:20 -0400

    Up to Enrich the Session With Verified Email ID

--- src/cphome/settings.py
+++ src/cphome/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 from decouple import config
 
+BASE_URL = config("BASE_URL", default="http://127.0.0.1:8000")
 # default backend
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config("EMAIL_HOST", cast=str, default=None)
--- src/emails/services.py
+++ src/emails/services.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.utils import timezone
 from .models import Email, EmailVerificationEvent
 
 EMAIL_HOST_USER = settings.EMAIL_HOST_USER
@@ -11,9 +12,10 @@ def verify_email(email):
 def get_verification_email_msg(verification_instance, as_html=False):
     if not isinstance(verification_instance, EmailVerificationEvent):
         return None
+    verify_link = verification_instance.get_link()
     if as_html:
-        return f"<h1>{verification_instance.id}</h1>"
-    return f"{verification_instance.id}"
+        return f"<h1>Verify your email with the following</h1><p><a href='{verify_link}'>{verify_link}</a></p>"
+    return f"Verify your email with the following:\n{verify_link}"
 
 def start_verification_event(email):
     email_obj, created = Email.objects.get_or_create(email=email)
@@ -42,3 +44,34 @@ def send_verification_email(verify_obj_id):
         fail_silently=False,
         html_message=text_html
     )
+    
+def verify_token(token, max_attempts=5):
+    qs = EmailVerificationEvent.objects.filter(token=token)
+    if not qs.exists() and not qs.count() == 1:
+        return False, "Invalid token"
+    """
+    Has token
+    """
+    has_email_expired = qs.filter(expired=True)
+    if has_email_expired.exists():
+        """ token expired """
+        return False, "Token expired, try again"
+    """
+    Has token, not expired
+    """
+    max_attemps_reached = qs.filter(attempts__gte=max_attempts)
+    if max_attemps_reached.exists():
+        """ update max attempts + 1"""
+        # max_attemps_reached.update()
+        return False, "Token expired, used too many times"
+    """ Token valid """
+    """update attempts, expire token if attempts > max """
+    obj = qs.first()
+    obj.attempts += 1
+    obj.last_attempt_at = timezone.now()
+    if obj.attempts > max_attempts:
+        """ invalidation process """
+        obj.expired = True
+        obj.expired_at = timezone.now()
+    obj.save()
+    return True, "Welcome"

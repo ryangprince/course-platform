7e6581c9fcf118af47cf6c8f422b36ac8b4e032d
ryangprince <rpprince18@gmail.com>
2025-04-23 14:49:01 -0400

    Up to Using HTMX to Handle Logout

--- src/emails/views.py
+++ src/emails/views.py
@@ -1,8 +1,36 @@
+from django.conf import settings
 from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django_htmx.http import HttpResponseClientRedirect
 
 from . import services
+
+from .forms import EmailForm
+
+EMAIL_ADDRESS = settings.EMAIL_ADDRESS
+
+def email_token_login_view(request):
+    if not request.htmx:
+        return redirect('/')
+    email_id_in_session = request.session.get('email_id')
+    template_name = "emails/hx/form.html"
+    form = EmailForm(request.POST or None)
+    context = {
+        "form": form,
+        "message": "",
+        "show_form": not email_id_in_session
+    }
+    if form.is_valid():
+        email_val = form.cleaned_data.get('email')
+        obj = services.start_verification_event(email_val)
+        context['form'] = EmailForm()
+        context['message'] = f"Success! Check your email for verification from {EMAIL_ADDRESS}"
+        # return HttpResponseClientRedirect('/check-your-email')
+        return render(request, template_name, context)
+    else:
+        print(form.errors)
+    return render(request, template_name, context)
 
 # Create your views here.
 def verify_email_token_view(request, token, *args, **kwargs):
